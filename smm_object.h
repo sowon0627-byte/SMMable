3708951506fd5058ca590a080b8a8c9050d14c7b
sowon0627-byte <sowon0627@sookmyung.ac.kr>
2025-12-19 03:49:01 +0900

    Update smm_object.h

--- smm_object.h
+++ smm_object.h
@@ -18,6 +18,18 @@
     festival
 */
 
+#define SMMNODE_TYPE_LECTURE      0
+#define SMMNODE_TYPE_RESTAURANT   1
+#define SMMNODE_TYPE_LABORATORY   2
+#define SMMNODE_TYPE_HOME         3
+#define SMMNODE_TYPE_GOTOLAB      4
+#define SMMNODE_TYPE_FOODCHANGE   5
+#define SMMNODE_TYPE_FESTIVAL     6
+
+#define SMMNODE_OBJTYPE_BOARD   0
+#define SMMNODE_OBJTYPE_GRADE   1
+#define SMMNODE_OBJTYPE_FOOD    2
+#define SMMNODE_OBJTYPE_FEST    3
 
 /* grade :
     A+,
@@ -31,15 +43,15 @@
     C-
 */
 
-
+#define SMMNODE_MAX_GRADE       13
 
 //object generation
-int smmObj_genNode(char* name, int type, int credit, int energy);
-char* smmObj_getName(int node_nr);
-int smmObj_getType(int node_nr);
-int smmObj_getEnergy(int node_nr);
-char* smmObj_getNodeName(int node_type);
-
+void* smmObj_genObject(char* name, int objType, int type, int credit, int energy, int grade);
+char* smmObj_getObjectName(void *ptr);
+int smmObj_getNodeType(void *ptr);
+int smmObj_getObjectEnergy(void *ptr);
+char* smmObj_getTypeName(int node_type);
+int smmObj_getNodeCredit(int node_nr);
 //member retrieving
 
 
